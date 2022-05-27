3ddaef829e3fecd17aa1c17d6d7a78a446846e54
Peter <peter.buschenreiter@gmail.com>
2022-05-27 13:40:35 +0200

    login script
    Took 25 minutes

--- scripts/login.js
+++ scripts/login.js
@@ -1,18 +1,17 @@
-import {gamesJSON} from "../rest/games";
-import {usersJSON} from "../rest/users";
+import {gamesJSON} from "../rest/games.js";
+import {usersJSON} from "../rest/users.js";
 
 const games = JSON.parse(gamesJSON);
 const users = JSON.parse(usersJSON);
 
 const username = document.getElementById("username");
 const password = document.getElementById("password");
-const form = document.getElementById("form");
+const form = document.getElementById("login");
 
-form.addEventListener("submit", (e) => {
+form.addEventListener("submit", e => {
 	e.preventDefault();
-    getInfo();
+	getInfo(username, password);
 });
-
 
 
 // const {userJSON} = require("user");
@@ -21,12 +20,11 @@ form.addEventListener("submit", (e) => {
 
 
 function getInfo() {
-	let name = document.getElementById("name").value;
+	let name = document.getElementById("username").value;
 	let password = document.getElementById("password").value;
-	let user_role = document.getElementById("user_role").value;
 
 	for (let i = 0; i < users.length; i++) {
-		if (username === users[i].username && password === users[i].password) {
+		if (name === users[i].username && password === users[i].password) {
 			console.log(username + "is logged in");
 		} else {
 			console.log("incorrect username or password.");
